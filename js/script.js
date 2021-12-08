96d3baff07e1ec08b8d69c36b84047326a01b1cb
00015272 <wiut00015272@gmail.com>
2021-12-08 18:45:10 +0500

    Proper Text added

--- js/script.js
+++ js/script.js
@@ -14,3 +14,16 @@ window.onscroll = function() {
     navbar.classList.remove('active');
 }
 
+fetch("https://us-doctors-and-medical-professionals.p.rapidapi.com/search_npi?npi=1033112214", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "us-doctors-and-medical-professionals.p.rapidapi.com",
+		"x-rapidapi-key": "SIGN-UP-FOR-KEY"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
